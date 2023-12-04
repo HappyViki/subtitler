ecdbc4c484624fcf701734d5b6b2e201ee6f0164
Veronica Eulenberg <veronica.eulenberg@gmail.com>
2023-12-03 18:51:31 -0600

    migrate to express and separate code

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('template'))
+
+app.use(express.static('public'))
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
